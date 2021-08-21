8e3890b5a699756902fb0f610d767e74ee744b09
frankjmontero <frankjmontero@FrankAcerAllPurpose.localdomain>
2021-08-20 23:51:01 -0400

    N_Fibonacci Number: JS v2.

--- Numbers/N_Fibonacci_Number/N_Fibonacci_Number.js
+++ Numbers/N_Fibonacci_Number/N_Fibonacci_Number.js
@@ -1,12 +1,16 @@
-let fibSec1 = n => {
+let fibSec = n => {
   let a = 0, b = 1;
   for(let i = 1; i < n; i++) {
-    [a, b] = [b, a + b];
+    a += b;
+    b = a - b;
+    a -= b;
+    b += a;
   }
+  
   return b;
 }
 
-console.log(fibSec1(1) === 1);
-console.log(fibSec1(5) === 5);
-console.log(fibSec1(7) === 13);
-console.log(fibSec1(8) === 21);
+console.log(fibSec(1) === 1);
+console.log(fibSec(5) === 5);
+console.log(fibSec(7) === 13);
+console.log(fibSec(8) === 21);
