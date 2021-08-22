565ffb928c59e236ecb8d6f4d83934c61cd6943e
frankjmontero <frankjmontero@FrankAcerAllPurpose.localdomain>
2021-08-22 11:53:05 -0400

    Steps with pound sign: - JS v2.

--- String/Steps_With_#/Steps.js
+++ String/Steps_With_#/Steps.js
@@ -1,7 +1,6 @@
 const drawSteps = hight => {
   for (let i = 1, j = hight - 1; i <= hight; i++, j--) {
-    console.log('\'' + '#'.repeat(i) + ' '.repeat(j) + '\'');
+    console.log(`'${'#'.repeat(i)}${' '.repeat(j)}'`);
   }
-};
-
+}
 drawSteps(5);
