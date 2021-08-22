b05c8cd54bd92d2a6c01f0800b7156cf506affd4
frankjmontero <frankjmontero@FrankAcerAllPurpose.localdomain>
2021-08-22 16:39:18 -0400

    Sum of odd numbers triangle row: - JS v2.

--- Numbers/Odd_Triangle_Sum/TriangleSum.js
+++ Numbers/Odd_Triangle_Sum/TriangleSum.js
@@ -1,13 +1,4 @@
-const sumRow = r => {
-  if (r === 1) return r;
-  let prev = r * (r-1) + 1, sum = prev;
-  
-  for (let i = 1; i < r; i++) {
-    sum += prev += 2;
-  }
-
-  return sum;
-}
+const sumRow = r => r ** 3;
 
 console.log(sumRow(1) === 1);
 console.log(sumRow(4) === 64);
