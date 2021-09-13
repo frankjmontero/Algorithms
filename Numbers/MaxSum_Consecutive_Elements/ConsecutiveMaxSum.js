da5faeae32fa608b34c3cd57622d300d726a065d
frankjmontero <frankjmontero@FrankAcerAllPurpose.localdomain>
2021-09-12 21:06:44 -0400

    Consecutive maximum sum: - JS v2

--- Numbers/MaxSum_Consecutive_Elements/ConsecutiveMaxSum.js
+++ Numbers/MaxSum_Consecutive_Elements/ConsecutiveMaxSum.js
@@ -2,22 +2,22 @@ const highestConsecutiveSum = (arr, k) => {
   highestSum = 0;
   sumIndex = 0;
 
-  for(let i = 0; i < arr.length - k; i++) {
-    let sum = 0;
-    
-    for(let j = 0; j < k; j++) {
-      sum += arr[i+j];
-    }
+  let sum = 0;
+  
+  for(let i = 0; i < arr.length; i++) {
+    if (i < k) {
+      sum += arr[i];
+    } else sum = sum - arr[i-k] + arr[i];
 
     if(sum > highestSum) {
       highestSum = sum;
-      sumIndex = i;
+      sumIndex = i + 1;
     }
   }
 
   console.log(`  Highest possible sum: ${highestSum}
-  Elements added: ${arr.slice(sumIndex, sumIndex+k)}`);
+  Elements added: ${arr.slice(sumIndex - k, sumIndex)}`);
 }
 
-highestConsecutiveSum([1,3,7,2,4,1],3);
+highestConsecutiveSum([1,3,7,2,4,1],4);
 highestConsecutiveSum([150, 200, 300, 7, 8, 1, 2, 8, 10], 3);
